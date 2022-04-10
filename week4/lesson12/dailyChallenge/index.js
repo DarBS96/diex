dabc7bf94e617c8dcb53506da4ac766abdb76c5b
Dar BS <bsdar00@gmail.com>
2022-04-10 20:05:13 +0300

    update to Class's answer

--- week4/lesson12/dailyChallenge/index.js
+++ week4/lesson12/dailyChallenge/index.js
@@ -1,30 +1,50 @@
 // Daily Challenge: Planets;
-// 1:
 
-const planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn'];
+let planets = [
+    { name: "Venus", moons: [] },
+    { name: "Earth", moons: ["moon"] },
+    { name: 'Mars', moons: ["Deimos", "Phobos"] },
+    { name: 'Jupiter', moons: ["IO", "Europa", "Ganymede"] },
+    { name: 'Sarturna', moons: ["Ymir", "Paaliaq", "Siarnaq", "Tarvos", "Kiviuq", "Ijiraq", "Thrymr"] },
+    { name: 'Uranus', moons: ["Juliette", "Portia", "Cressida", "Desdémone", "Rosalinde", "Belinda", "Cordélia"] },
+    { name: 'Neptune', moons: ["Despina", "Galatea", "HippoCamp"] }
+];
 const section = document.querySelector('section');
 
-// 2 + 3 + 4:
 const getRandomColor = () => {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16); // toString base 16 convert to f (css tricks)
     return "#" + randomColor;
 };
-planets.forEach((planet) => {
-    const createDiv = document.createElement('div');
-    createDiv.className = 'planet';
-    createDiv.innerHTML = planet;
-    section.appendChild(createDiv);
-    createDiv.style.backgroundColor = getRandomColor();
-});
 
-//Bonus
-const moons = { Mercury: 3, Venus: 2, Earth: 4, Mars: 2, Jupiter: 1, Saturn: 1 };
-for (planet in moons) {
-    const createDiv = document.createElement('div');
-    createDiv.className = 'moon';
-    createDiv.innerHTML = planet;
-    section.appendChild(createDiv);
-    createDiv.style.backgroundColor = getRandomColor();
+const elementToPush = (element) => {
+    section.appendChild(element);
 };
 
-section.style.display = 'flex'
+const createMoon = ((moon, index) => {
+    const createMoon = document.createElement('div');
+    createMoon.classList.add('moon');
+    createMoon.style.backgroundColor = getRandomColor();
+    let name = document.createTextNode(moon);
+    createMoon.appendChild(name);
+    console.log(index)
+    let leftVal = 140 + index * 80;
+    console.log(leftVal);
+    createMoon.style.left = (`${leftVal}px`);
+
+    return createMoon;
+});
+
+const createPlanet = (planet) => {
+    const createPlanet = document.createElement('div');
+    createPlanet.classList.add('planet');
+    // console.log(createPlanet);
+    createPlanet.style.backgroundColor = getRandomColor();
+    let name = document.createTextNode(planet.name);
+    createPlanet.appendChild(name);
+    planet.moons.forEach((moon, index) => {
+        createPlanet.appendChild(createMoon(moon, index));
+    });
+    return createPlanet;
+};
+
+planets.forEach((planet) => elementToPush(createPlanet(planet)));;
