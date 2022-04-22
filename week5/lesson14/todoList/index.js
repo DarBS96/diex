46e1b3ace08097fc8c34b559a66faf63cf4f2afa
Dar BS <bsdar00@gmail.com>
2022-04-22 16:18:41 +0300

    update todo list

--- week5/lesson14/todoList/index.js
+++ week5/lesson14/todoList/index.js
@@ -1,57 +1,44 @@
-let arrListTasks = [];
 let icon;
-let newTask;
 
-let listTasks = document.querySelector('.listTasks');
+const ul = document.querySelector('ul');
 const inputText = document.querySelector('input');
 
-let task = {
-    task_id: 0,
-    text: inputText.value,
-    done: false
-};
 
 const addTask = (e) => {
     e.preventDefault();
     if (!inputText.value) {
         alert('Must insert a task');
     } else {
-        let newTask = createTask();
-        arrListTasks.push(newTask);
-        console.log(arrListTasks);
+        createTask();
     }
 };
 const createTask = () => {
-    // div second container
-    taskDiv = createElement("div");
-    taskDiv.className = 'task';
-    appendChild(listTasks, taskDiv);
+    // create li
+    const li = createElement("li");
+    li.className = 'task';
+    ul.append(li);
 
     // create icon 
     icon = createElement("i");
     icon.className = 'fa-solid fa-xmark icon';
-    appendChild(taskDiv, icon);
-    console.log(icon);
+    appendChild(li, icon);
 
     // create input check
     let input = createElement("INPUT");
     input.setAttribute("type", "checkbox");
-    // input.className = 'checkbox-input';
-
-    appendChild(taskDiv, input);
+    appendChild(li, input);
 
     //create label to input check
     let newLabel = createElement("Label");
-    newLabel.setAttribute("for", task.task_id);
+    newLabel.setAttribute("for", inputText);
     newLabel.innerHTML = inputText.value;
     newLabel.className = 'strikethrough';
 
-    appendChild(taskDiv, newLabel);
+    appendChild(li, newLabel);
 
     inputText.value = '';
-    deleteTask(icon, taskDiv);
-
-    return taskDiv;
+    appendChild(ul, li);
+    deleteTask(icon);
 };
 
 const createElement = (element) => {
@@ -59,13 +46,13 @@ const createElement = (element) => {
 };
 
 const appendChild = (elem1, elem2) => {
-    console.log(elem1.appendChild(elem2));
+    elem1.appendChild(elem2);
 };
 const btn = document.querySelector('button');
 btn.addEventListener('click', addTask);
 
-const deleteTask = (icon, task) => {
+const deleteTask = (icon) => {
     icon.addEventListener('click', function(e) {
-        task.remove();
+        console.log(this.parentElement.remove())
     });
 };
