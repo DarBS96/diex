e97f7ffdb4ca6ee58906add27e265a0e4cbfca16
Dar Ben Shitrit <bsdar00@gmail.com>
2022-05-03 19:23:37 +0300

    ypdate daily challenge of day 5

---
+++ week7/day5/dailyChallenge/index.js
@@ -0,0 +1,27 @@
+// Daily Challenge: Anagram;
+const isAnagram = (str1, str2) => {
+    let anagram = false;
+    let lowerCaseStr1 = str1.toLowerCase().replaceAll(' ', '');
+    let lowerCaseStr2 = str2.toLowerCase().replaceAll(' ', '');
+
+    let arrStr1 = [...lowerCaseStr1];
+    let arrStr2 = [...lowerCaseStr2];
+
+    let arr1 = arrStr1.reduce((total, nextLetter) => {
+        total[nextLetter] ? total[nextLetter]++ : total[nextLetter] = 1;
+        return total;
+    }, {});
+    let arr2 = arrStr2.reduce((total, nextLetter) => {
+        total[nextLetter] ? total[nextLetter]++ : total[nextLetter] = 1;
+        return total;
+    }, {});
+
+    if (arrStr1.length === arrStr2.length) {
+        if (arrStr1.every(letter => arrStr2.includes(letter))) {
+            arrStr1.forEach(letter => {
+                arr1[letter] === arr2[letter] ? anagram = true : anagram = false
+            });
+        }
+    }
+    anagram ? console.log(str1, 'is an anagram of', str2) : console.log(str1, 'is not an anagram of', str2);
+};
