98bf9f844b965701a082bb9d6443a3acdbab6647
Dar Ben Shitrit <bsdar00@gmail.com>
2022-05-17 20:04:11 +0300

    update exs of day2

---
+++ week9/day2/dailyChallenge/index.js
@@ -0,0 +1,107 @@
+// Daily Challenge: Play With Words;
+
+// 1rst Daily Challenge;
+
+const makeAllCaps = (arr) => {
+    return new Promise((resolve, reject) => {
+        arr.forEach(element => {
+            typeof(element) === 'string' ? resolve(arr.map(elem => elem.toUpperCase())): reject('Not all the words are type of string')
+        });
+    })
+}
+
+const sortWords = (upperCaseArr) => {
+    return new Promise((resolve, reject) => {
+        upperCaseArr.length > 4 ? resolve(upperCaseArr.sort()) : reject('Sorry the length of the words is less than 4')
+    })
+}
+
+makeAllCaps(["apple", "pear", "banana", "melon", "kiwi"])
+    .then(arr => sortWords(arr))
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
+
+// 2nd Daily Challenge;
+
+let morse = `{
+  "0": "-----",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  "a": ".-",
+  "b": "-...",
+  "c": "-.-.",
+  "d": "-..",
+  "e": ".",
+  "f": "..-.",
+  "g": "--.",
+  "h": "....",
+  "i": "..",
+  "j": ".---",
+  "k": "-.-",
+  "l": ".-..",
+  "m": "--",
+  "n": "-.",
+  "o": "---",
+  "p": ".--.",
+  "q": "--.-",
+  "r": ".-.",
+  "s": "...",
+  "t": "-",
+  "u": "..-",
+  "v": "...-",
+  "w": ".--",
+  "x": "-..-",
+  "y": "-.--",
+  "z": "--..",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "!": "-.-.--",
+  "-": "-....-",
+  "/": "-..-.",
+  "@": ".--.-.",
+  "(": "-.--.",
+  ")": "-.--.-"
+}`
+
+const toJs = (morse) => {
+    return new Promise((resolve, reject) => {
+        morse ? resolve(JSON.parse(morse)) : reject('Empty morse')
+    })
+}
+
+const toMorse = (morseObj) => {
+    return new Promise((resolve, reject) => {
+        const valuesArr = []
+        const userAnswer = prompt('Write a word or a sentence');
+        const keysArr = Object.entries(morseObj);
+        keysArr.forEach(([key, value]) => {
+            if (userAnswer.includes(key)) {
+                valuesArr.push(value)
+                resolve(valuesArr)
+            }
+        })
+        reject('Sorry the word doesnt exist in the morse')
+    })
+};
+
+const joinWords = (morseTranslation) => {
+    morseTranslation.forEach(val => {
+        const div = document.createElement('div')
+        div.textContent = val
+        document.body.append(div)
+    })
+}
+
+toJs(morse)
+    .then(result => toMorse(result)
+        .then(result => joinWords(result))
+        .catch(err => console.log(err)))
+    .catch(err => console.log(err));
---
+++ week9/day2/exmandatory/index.js
@@ -0,0 +1,49 @@
+// Exercises XP;
+
+// Exercise 1 : Comparison;
+
+const compareToTen = (num) => {
+    return new Promise((resolve, reject) => {
+        num < 10 ? resolve(`The number is less then 10: ${num}`) :
+            reject(`The number is greater then 10: ${num}`)
+    })
+}
+
+compareToTen(15)
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
+
+
+compareToTen(8)
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
+
+// Exercise 2 : Promises;
+
+// 1:
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('success')
+    }, 4000)
+})
+
+promise.then(result => console.log(result))
+    .catch(err => console.log('Ooops something went wrong'))
+
+// 2:
+
+const shorterPromise = Promise.resolve('success')
+shorterPromise.then(result => setTimeout(() => console.log(result), 4000))
+    .catch(err => console.log('Ooops something went wrong'))
+
+// Exercise 3 : Resolve & Reject
+
+// 1:
+
+resolvePromise = Promise.resolve(3)
+resolvePromise.then(result => console.log(result))
+
+// 2:
+
+rejectPromise = Promise.reject('Boo')
+rejectPromise.catch(result => console.log(result));
