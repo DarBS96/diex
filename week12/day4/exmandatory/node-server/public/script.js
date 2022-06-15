2cba1fe5db3efcb13ee9ad462ec3cd94d1f57127
Dar Ben Shitrit <bsdar00@gmail.com>
2022-06-15 22:00:35 +0300

    .

--- week12/day4/exmandatory/node-server/public/script.js
+++ week12/day4/exmandatory/node-server/public/script.js
@@ -1,9 +1,9 @@
 const getData = async () => {
   try {
-    const res = await fetch("http://localhost:3000/");
+    const res = await fetch("http://localhost:3000/api");
     console.log(res);
     const data = await res.json();
-    // console.log(data);
+    console.log(data);
   } catch (e) {
     console.log(e.message);
   }
