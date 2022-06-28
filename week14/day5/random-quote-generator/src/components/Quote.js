93db53d6702d1572768c5665425876084820ac2f
Dar Ben Shitrit <bsdar00@gmail.com>
2022-06-28 20:23:30 +0300

    .

--- week14/day5/random-quote-generator/src/components/Quote.js
+++ week14/day5/random-quote-generator/src/components/Quote.js
@@ -1,43 +1,78 @@
 import quotes from "../quotesDB";
-import { useState } from "react";
+import React from "react";
+// import { useState } from "react";
 import "./quote.css";
 
-export const Quote = () => {
-  const colors = [
-    "secondary",
-    "success",
-    "primary",
-    "danger",
-    "warning",
-    "info",
-  ];
+const colors = ["secondary", "success", "primary", "danger", "warning", "info"];
 
-  const [color, setColor] = useState(colors[0]);
-  console.log(color);
-  const [quote, setQuote] = useState(`"${quotes[0].quote}"`);
-  const [author, setAuthor] = useState(`-${quotes[0].author}-`);
+// export const Quote = () => {
 
-  const onClickChanged = () => {
+//   const [color, setColor] = useState(colors[0]);
+//   console.log(color);
+//   const [quote, setQuote] = useState(`"${quotes[0].quote}"`);
+//   const [author, setAuthor] = useState(`-${quotes[0].author}-`);
+
+//   const onClickChanged = () => {
+//     const randomQuotesNum = Math.floor(Math.random() * quotes.length + 1);
+//     const randomColorsNum = Math.floor(Math.random() * colors.length + 1);
+//     setQuote(`"${quotes[randomQuotesNum].quote}"`);
+//     setAuthor(`-${quotes[randomQuotesNum].author}-`);
+//     setColor(colors[randomColorsNum]);
+//   };
+//   return (
+//     <>
+//       <main className={`vh-100 bg-${color} d-grid`}>
+//         <div className={`bg-white modalQuote`}>
+//           <h1 className={`text-${color}`}>{quote}</h1>
+//           <p className={`text-${color} `}>{author}</p>
+//           <button
+//             className={`btn text-white bg-${color}`}
+//             onClick={onClickChanged}
+//           >
+//             New quote
+//           </button>
+//         </div>
+//       </main>
+//     </>
+//   );
+// };
+
+export class Quote extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      quote: quotes[0].quote,
+      color: colors[0],
+      author: quotes[0].author,
+    };
+  }
+
+  onClickChanged = () => {
     const randomQuotesNum = Math.floor(Math.random() * quotes.length + 1);
     const randomColorsNum = Math.floor(Math.random() * colors.length + 1);
-    setQuote(`"${quotes[randomQuotesNum].quote}"`);
-    setAuthor(`-${quotes[randomQuotesNum].author}-`);
-    setColor(colors[randomColorsNum]);
+    this.setState({
+      quote: `"${quotes[randomQuotesNum].quote}"`,
+      author: `-${quotes[randomQuotesNum].author}-`,
+      color: colors[randomColorsNum],
+    });
   };
-  return (
-    <>
-      <main className={`vh-100 bg-${color} d-grid`}>
-        <div className={`bg-white modalQuote`}>
-          <h1 className={`text-${color}`}>{quote}</h1>
-          <p className={`text-${color} `}>{author}</p>
-          <button
-            className={`btn text-white bg-${color}`}
-            onClick={onClickChanged}
-          >
-            New quote
-          </button>
-        </div>
-      </main>
-    </>
-  );
-};
+
+  render() {
+    return (
+      <>
+        <main className={`vh-100 bg-${this.state.color} d-grid`}>
+          <div className={`bg-white modalQuote`}>
+            <h1 className={`text-${this.state.color}`}>{this.state.quote}</h1>
+            <p className={`text-${this.state.color} `}>{this.state.author}</p>
+            <button
+              className={`btn text-white bg-${this.state.color}`}
+              onClick={this.onClickChanged}
+            >
+              New quote
+            </button>
+          </div>
+        </main>
+      </>
+    );
+  }
+}
