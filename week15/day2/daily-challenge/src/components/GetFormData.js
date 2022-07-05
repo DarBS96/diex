a17a2dcc60f6a2b5f8ceced87508eafafe004997
Dar Ben Shitrit <bsdar00@gmail.com>
2022-07-05 12:52:56 +0300

    .

--- week15/day2/daily-challenge/src/components/GetFormData.js
+++ week15/day2/daily-challenge/src/components/GetFormData.js
@@ -45,56 +45,58 @@ function GetFormData(props) {
     <div style={{ backgroundColor: "grey" }} className="containerGetFormData">
       <h1 className="title">Please fill up your trip's data</h1>
       <main className="mainGetFormData">
-        <div className="field">
-          <label>First Name</label>
-          <input
-            type="text"
-            name="firstName"
-            onChange={handleChange}
-            className="textInput"
-          />
-        </div>
-        <div className="field">
-          <label>Last Name</label>
-          <input type="text" name="lastName" onChange={handleChange} />
-        </div>
-        <div className="field">
-          <label>Age</label>
-          <input type="number" name="age" onChange={handleChange} />
-        </div>
-        <div className="field">
-          <label>Male</label>
-          <input type="checkbox" name="male" onChange={handleChange} />
-        </div>
-        <div className="field">
-          <label>Female</label>
-          <input type="checkbox" name="female" onChange={handleChange} />
-        </div>
-        <div className="field">
-          <label htmlFor="destination">select your destination</label>
-          <select name="destination" id="destination" onChange={handleChange}>
-            <option value="Thailand">Thailand</option>
-            <option value="London">London</option>
-            <option value="Italy">Italy</option>
-          </select>
-        </div>
-        <div className="field">
-          <label>Dietary Restrictions</label>
-          <ul style={{ listStyle: "none" }}>
-            <li>
-              <input type="checkbox" onChange={handleChange} />
-              Nuts free
-            </li>
-            <li>
-              <input type="checkbox" onChange={handleChange} />
-              Lactose free
-            </li>
-            <li>
-              <input type="checkbox" onChange={handleChange} />
-              Vegan
-            </li>
-          </ul>
-        </div>
+        <form action="ui form">
+          <div className="field">
+            <label>First Name</label>
+            <input
+              type="text"
+              name="firstName"
+              onChange={handleChange}
+              className="textInput"
+            />
+          </div>
+          <div className="field">
+            <label>Last Name</label>
+            <input type="text" name="lastName" onChange={handleChange} />
+          </div>
+          <div className="field">
+            <label>Age</label>
+            <input type="number" name="age" onChange={handleChange} />
+          </div>
+          <div className="field">
+            <label>Male</label>
+            <input type="checkbox" name="male" onChange={handleChange} />
+          </div>
+          <div className="field">
+            <label>Female</label>
+            <input type="checkbox" name="female" onChange={handleChange} />
+          </div>
+          <div className="field">
+            <label htmlFor="destination">select your destination</label>
+            <select name="destination" id="destination" onChange={handleChange}>
+              <option value="Thailand">Thailand</option>
+              <option value="London">London</option>
+              <option value="Italy">Italy</option>
+            </select>
+          </div>
+          <div className="field">
+            <label>Dietary Restrictions</label>
+            <ul style={{ listStyle: "none" }}>
+              <li>
+                <input type="checkbox" onChange={handleChange} />
+                Nuts free
+              </li>
+              <li>
+                <input type="checkbox" onChange={handleChange} />
+                Lactose free
+              </li>
+              <li>
+                <input type="checkbox" onChange={handleChange} />
+                Vegan
+              </li>
+            </ul>
+          </div>
+        </form>
       </main>
       <button className="submitBtn">Submit</button>
       <br />
